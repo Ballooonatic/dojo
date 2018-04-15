55a5e9b81f6ed596170ec7c5da5c517206add568
Michael Imgrund <michael.imgrund@gmail.com>
2018-04-15 01:02:34 -0400

    first js assignment. variable practice and notes added.

---
+++ web-fundamentals/javascript/i-do-declare.js
@@ -0,0 +1,17 @@
+var num = 2,
+    neg = -3,
+    dec = 0.984,
+    mil = 12304989,
+    
+    ayy = "ayy",
+    lmao = "lmao",
+    chars = "!@#$%^&))(*&[]|-=+_~`~////;,';.<>:'}{",
+
+    tru = true,
+    fals = false;
+
+var var_list = [num, neg, dec, mil, ayy, lmao, chars, tru, fals];
+
+var_list.forEach(element => {
+    console.log(element);
+});

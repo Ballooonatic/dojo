eff2b2a1b16aff89c9a0299807f398ffb1f33e1e
Michael Imgrund <michael.imgrund@gmail.com>
2018-04-16 00:14:29 -0400

    modified for-a-few-billion for the bonus Q's

--- web-fundamentals/javascript/for-a-few-billion.js
+++ web-fundamentals/javascript/for-a-few-billion.js
@@ -1,8 +1,16 @@
 var pennies = 0.01;
 
-for (let i = 1; i < 30; i++) {
+for (let i = 2; i < Infinity; i++) {
     pennies *= 2;
-    console.log("ya got",pennies,"dollas after",i,"days.");
+    console.log("ya got",pennies,"dollas after",i,"day(s).");
+    if (pennies == Infinity) {
+        break;
+    }
 }
 
 console.log("the total amount is",pennies,"monet after 30 days.");
+
+// BONUS
+// it takes 21 days to make over $10,000,
+// only 28 to break a million,
+// and 1032 or so to reach infinity

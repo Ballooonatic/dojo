b6a0e0ac9565b0da0f0ed80501927df71421e247
Michael Imgrund <michael.imgrund@gmail.com>
2018-05-02 16:15:35 -0400

    added math-module, and some modularizing notes

---
+++ MEAN/node/math-module/app.js
@@ -0,0 +1,6 @@
+var mathlib = require('./mathlib.js')();
+
+console.log(mathlib.add(3, 6));
+console.log(mathlib.multiply(3, 6));
+console.log(mathlib.square(3));
+console.log(mathlib.random(3, 6));
